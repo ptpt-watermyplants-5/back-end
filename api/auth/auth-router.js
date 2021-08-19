590996144c2f3f475c2a7038a944a46192ae46cc
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-19 05:08:25 -0400

    auth endpoints & jwt

---
+++ api/auth/auth-router.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../user/user-model');
+const {
+    validRequest,
+    validUsername,
+} = require('../middlewares/auth-middleware');
+const tokenBuilder = require('./tokenBuilder');
+
+router.post('/register', validRequest, validUsername, (req, res, next) => {
+    const user = req.body;
+
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(user.password, rounds);
+
+    user.password = hash;
+
+    User.addUser(user)
+    .then(newUser => {
+        res.status(201).json(newUser)
+    })
+    .catch(err => next(err))
+});
+
+router.post('/login', validRequest, (req, res, next) => {
+    const { username, password } = req.body;
+
+    User.findBy(username)
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = tokenBuilder(user);
+  
+            res.status(200).json({
+              message: `welcome, ${user.username}`,
+              token: token,
+            });
+          }else {
+            next({ status: 401, message: "invalid credentials" })
+          }
+    })
+    .catch(err => next(err))
+});
+
+module.exports = router;
---
+++ api/auth/tokenBuilder.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('./secrets');
+
+module.exports = function (user) {
+    const payload = {
+        sub: user.id,
+        username: user.username,
+    }
+
+    const options = {
+        expiresIn: '1d',
+    }
+
+    return jwt.sign(payload, jwtSecret, options);
+};
