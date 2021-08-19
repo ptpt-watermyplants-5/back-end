3f19ef8c127f244fa2859178c2b3f004625841cc
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-19 03:08:42 -0400

    express server

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res, next) => {
+    next({status: 200, api: 'up'})
+});
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
+module.exports = server;
