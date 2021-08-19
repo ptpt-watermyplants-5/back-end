63b688cf98c6730cc5314eb2d8ce5617549b937e
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-19 05:02:02 -0400

    api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const authRouter = require('./auth/auth-router');
 
 const server = express();
 
@@ -8,8 +9,10 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
-server.get('/', (req, res, next) => {
-    next({status: 200, api: 'up'})
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up' });
 });
 
 server.use((err, req, res, next) => {
