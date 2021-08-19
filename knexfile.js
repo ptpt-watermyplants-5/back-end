84f9889e68c4e35033643a61bf91b7cd5b27473d
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-19 05:00:48 -0400

    knex configs, migrations, db initialized

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,18 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
+
 module.exports = {
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/database.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    ...sharedConfig,
+    connection: { filename: './data/database.db3' },
+    seeds: { directory: './data/seeds' },
   },
   production: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/database.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    ...sharedConfig,
+    connection: { filename: './data/database.db3' },
   },
 };
