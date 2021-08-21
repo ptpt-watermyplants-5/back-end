5cdf8c41dd1023d22f0dc2bcc4cb45ee07df73de
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-21 05:33:03 -0400

    db queries

--- api/user/user-model.js
+++ api/user/user-model.js
@@ -4,6 +4,10 @@ module.exports = {
     findBy,
     findById,
     addUser,
+    updateUser,
+    getUserPlants,
+    addPlant,
+    removePlant,
 };
 
 function findBy(username) {
@@ -11,10 +15,37 @@ function findBy(username) {
 }
 
 function findById(id) {
-    return db('users').where({ id }).first();
+    return db('users').where({ "user_id": id }).first();
 };
 
 async function addUser(user) {
     const [id] = await db('users').insert(user);
     return findById(id);
+};
+
+async function updateUser(id, user) {
+    await db('users').where({ "user_id": id }).update(user);
+    return findById(id);
+};
+
+async function getUserPlants(id) {
+    const user = await db('users').where({ "user_id": id }).first();
+    const plants = await db('plants').where({ "user_id": id });
+
+    return {
+        'user_id': user.user_id,
+        'username': user.username,
+        'plants': plants,
+    }
+};
+
+async function addPlant(uid, plant) {
+    plant.user_id = uid;
+    const [id] = await db('plants').insert(plant);
+
+    return db('plants').where({ "plant_id": id}).first();
+};
+
+function removePlant(id) {
+    return db('plants').where({ "plant_id": id}).del();
 };
