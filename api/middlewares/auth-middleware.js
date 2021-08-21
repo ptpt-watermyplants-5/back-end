af509079423236427b943632a8f3d9b7669b2689
Fry-Fr <ryjlwdg89@gmail.com>
2021-08-21 05:32:11 -0400

    restriction middlewares

--- api/middlewares/auth-middleware.js
+++ api/middlewares/auth-middleware.js
@@ -1,8 +1,14 @@
 const User = require('../user/user-model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../auth/secrets');
 
 module.exports = {
     validRequest,
     validUsername,
+    restricted,
+    verifyUser,
+    updatePasswordHash,
 };
 
 function validRequest(req, res, next) {
@@ -21,8 +27,49 @@ async function validUsername(req, res, next) {
     const valid = await User.findBy(username);
   
     if (valid) {
-      next({ status: 422, message: "username taken" });
+      next({ status: 422, message: 'username taken' });
     }else {
       next();
     }
+  };
+
+  function restricted(req, res, next) {
+    const token = req.headers.authorization;
+
+    if(!token) {
+      return next({ status: 401, message: 'requires authorization'})
+    }
+
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return next({ status: 401, message: 'authorization is invalid' })
+      }
+
+      req.decodedToken = decodedToken;
+      next();
+    });
+  };
+
+  function updatePasswordHash(req, res, next) {
+    const usrObj = req.body;
+
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(usrObj.password, rounds);
+
+    usrObj.password = hash;
+    req.body = usrObj;
+    next();
+  };
+
+  async function verifyUser(req, res, next) {
+    const { username } = req.decodedToken;
+    const { id } = req.params;
+    const verifiedUser = await User.findById(id);
+
+    if (Boolean(verifiedUser) === false) {
+      return next({ status: 404, message: `not found user id ${id}`})
+    }else if (username !== verifiedUser.username) {
+      return next({ status: 401, message: 'invalid authorization' })
+    }
+    next();
   };
